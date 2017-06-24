5f6855872d273d40b0c1e17e858dd4c4163087e1
Jakub Kulík <kulikjak@gmail.com>
2017-06-24 20:52:03 +0200

    Minor changes

--- main.c
+++ main.c
@@ -56,7 +56,7 @@ MENU* init_menus(WINDOW* menu_window) {
   // main menu strings
   count = ARRAY_SIZE(main_menu_strings);
   main_menu_items = (ITEM**) calloc (count, sizeof(ITEM*));
-  for(i = 0; i < count; ++i)
+  for (i = 0; i < count; ++i)
     main_menu_items[i] = new_item(main_menu_strings[i], NULL);
 
   main_menu = new_menu((ITEM**)main_menu_items);
@@ -64,7 +64,7 @@ MENU* init_menus(WINDOW* menu_window) {
   // missile menu strings
   count = ARRAY_SIZE(missile_menu_strings);
   missile_menu_items = (ITEM**) calloc (count, sizeof(ITEM*));
-  for(i = 0; i < count; ++i)
+  for (i = 0; i < count; ++i)
     missile_menu_items[i] = new_item(missile_menu_strings[i], missile_menu_desc[i]);
 
   missile_menu = new_menu((ITEM**)missile_menu_items);
@@ -73,7 +73,7 @@ MENU* init_menus(WINDOW* menu_window) {
   // log menu strings
   count = ARRAY_SIZE(log_menu_strings);
   log_menu_items = (ITEM**) calloc (count, sizeof(ITEM*));
-  for(i = 0; i < count; ++i)
+  for (i = 0; i < count; ++i)
     log_menu_items[i] = new_item(log_menu_strings[i], NULL);
 
   log_menu = new_menu((ITEM**)log_menu_items);
@@ -86,8 +86,10 @@ MENU* init_menus(WINDOW* menu_window) {
 }
 
 void free_menus() {
+  int i;
+
   free_menu(main_menu);
-  for(int i = 0; i < ARRAY_SIZE(main_menu_strings); ++i)
+  for (i = 0; i < ARRAY_SIZE(main_menu_strings); i++)
     free_item(main_menu_items[i]);
 }
 
@@ -125,7 +127,7 @@ void text_window(const char** text, int count) {
                        MENU_MARGIN_LEFT);
 
   wbkgd(text_window, COLOR_PAIR(1) | WA_BOLD);
-  scrollok(text_window ,TRUE);
+  scrollok(text_window, TRUE);
 
   for (int i = 0; i < count; i++) {
     print_text(text_window, text[i]);
@@ -134,16 +136,16 @@ void text_window(const char** text, int count) {
   }
 
   delwin(text_window);
-} 
+}
 
 void popup_window(char* message) {
   int window_width = MIN(100, w.ws_col - 20);
   WINDOW* popup_window = newwin(5, window_width, (w.ws_row - 7) / 2, (w.ws_col - window_width) / 2);
-    
+
   box(popup_window, 0, 0);
   wbkgd(popup_window, COLOR_PAIR(1) | WA_BOLD);
 
-  mvwprintw(popup_window, 5/2,(window_width - strlen(message))/2,"%s",message);   
+  mvwprintw(popup_window, 5/2, (window_width - strlen(message))/2, "%s", message);
   wrefresh(popup_window);
 
   getch();
@@ -159,7 +161,7 @@ int main(int argc, char* argv[]) {
   int c;
   MENU *current_menu;
   WINDOW *menu_window;
-  
+
   initscr();
   start_color();
 
@@ -200,8 +202,8 @@ int main(int argc, char* argv[]) {
 
   int choice;
 
-  while((c = wgetch(menu_window)) != KEY_F(1)) {
-    switch(c) {
+  while ((c = wgetch(menu_window)) != KEY_F(1)) {
+    switch (c) {
       case KEY_DOWN:
         menu_driver(current_menu, REQ_DOWN_ITEM);
         break;
@@ -213,27 +215,27 @@ int main(int argc, char* argv[]) {
         choice = item_index(current_item(current_menu));
 
         if (current_menu == main_menu) {
-          switch(choice) {
-            case 0: // [VAULT B42 INSTRUCTIONS] TODO
+          switch (choice) {
+            case 0:  // [VAULT B42 INSTRUCTIONS] TODO
               text_window(test_text, 2);
               break;
-            case 1: // [Nuclear warheads operation manual] TODO
+            case 1:  // [Nuclear warheads operation manual] TODO
               text_window(test_text, 2);
               break;
-            case 2: // [Nuclear missiles status]
+            case 2:  // [Nuclear missiles status]
               current_menu = missile_menu;
               break;
-            case 3: // [Main Base Communication Channel]
+            case 3:  // [Main Base Communication Channel]
               popup_window("Communication channel DISCONNECTED!");
               break;
-            case 4: // [Evacuation Instructions] TODO
+            case 4:  // [Evacuation Instructions] TODO
               text_window(test_text, 2);
               break;
-            case 5: // [Overseer's Log]
+            case 5:  // [Overseer's Log]
               current_menu = log_menu;
               break;
-            case 6: // [Exit Terminal]
-              // shutdown_pc();
+            case 6:  // [Exit Terminal]
+              /* shutdown_pc(); */
               free_menus();
               endwin();
               exit(EXIT_SUCCESS);
@@ -244,30 +246,31 @@ int main(int argc, char* argv[]) {
           post_menu(current_menu);
 
         } else if (current_menu == missile_menu) {
-          if (choice >= 0 && choice < 6)
+          if (choice >= 0 && choice < 6) {
             popup_window("Missile Launch bay is empty!");
-          else if (choice == 6)
+          } else if (choice == 6) {
             current_menu = main_menu;
-          else
+          } else {
             UNREACHABLE
+          }
 
         } else if (current_menu == log_menu) {
-          switch(choice) {
-            case 0: // TODO
+          switch (choice) {
+            case 0:  // TODO
               text_window(test_text, 2);
               break;
-            case 1: // TODO
+            case 1:  // TODO
               text_window(test_text, 2);
               break;
-            case 2: // [Back]
+            case 2:  // [Back]
               current_menu = main_menu;
               break;
             default:
               UNREACHABLE
           }
-
-        } else
+        } else {
           UNREACHABLE
+        }
 
         post_menu(current_menu);
         break;
@@ -275,6 +278,6 @@ int main(int argc, char* argv[]) {
         break;
     }
     wrefresh(menu_window);
-  } 
+  }
   UNREACHABLE
 }
